9ff0c1e9031597870bd0d00daa6e2dbff68f22f4
Stephen Matta <stephenjmatta+github@gmail.com>
2015-09-27 14:48:09 -0400

    Limiting published votes to those that happened today

--- server/PublishCollections.js
+++ server/PublishCollections.js
@@ -1,7 +1,8 @@
 Meteor.publish('Sites', () => Sites.find());
 Meteor.publish('Votes', function votePublish() {
   if (this.userId) {
-    return Votes.find({}, { fields: { user: 0 }});
+    const midnight = moment().startOf('day').toDate();
+    return Votes.find({ createdAt: { $gte: midnight } }, { fields: { user: 0 }});
   }
   this.ready();
 });
